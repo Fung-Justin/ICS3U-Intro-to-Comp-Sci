86bdda11912face1bb98415a0026058291685729
Justin Fung <jfung2@bayviewglen.ca>
2021-01-27 10:31:41 -0500

    Day 2, Print, Escape Sequences and Comments Homework

---
+++ Unit One-Introduction to Java/src/week1/ExampleTwo.java
@@ -0,0 +1,27 @@
+package week1;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+
+        // Letter "J"
+        System.out.println(" \t JJJJJJJJJJ");
+        System.out.println("\t     J \n\t     J");
+        System.out.println("\tJ    J");
+        System.out.println(" \t  JJJ");
+        
+        // Letter "A"
+        System.out.println("\n\t      A");
+        System.out.println("\t    A   A");
+        System.out.println("\t   A     A");
+        System.out.println("\t  AAAAAAAAA");
+        System.out.println("\t A         A \n\t A         A  ");
+
+        // Letter "C"
+        System.out.println(" \t   cc");
+        System.out.println(" \t  c   c");
+        System.out.println(" \t c \n \t c");
+        System.out.println(" \t  c   c");
+        System.out.println(" \t   cc");
+
+    }
+}
