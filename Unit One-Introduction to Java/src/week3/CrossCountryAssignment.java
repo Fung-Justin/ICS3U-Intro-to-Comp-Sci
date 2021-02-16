528f4a74fb4eca8ca0b8f5d8f8e1d01a949fe6c6
Justin Fung <jfung2@bayviewglen.ca>
2021-02-16 00:46:58 -0500

    Completed Comments and Minor changes to Cross Country Assignment

--- Unit One-Introduction to Java/src/week3/CrossCountryAssignment.java
+++ Unit One-Introduction to Java/src/week3/CrossCountryAssignment.java
@@ -3,67 +3,70 @@ package week3;
 import java.util.Scanner;
 
 public class CrossCountryAssignment {
-    static String mile3;
-     static String mile2;
-     static String mile1; 
-     static String fName;
-     static String lName; 
-     
+    static int runNum = 0; 
+
+    //this variable keeps track of the number of the runners and will increase by 1 every time runnerData is called in the program
 	public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         runnerData(in);
-        //runnerTime(mile1, mile2, mile3);
+        runnerData(in);
+        runnerData(in);
         
-        //runnerData(in);
-        //runnerTime(mile1,mile2,mile3);
-       
+        
     }
 
     
     
-
+    //converts the time which is in seconds(double), back into the normal time format, Min:sec.mili returns a String
     public static String getSplit( double m1){
-     String split = (m1/60) + "";
-     int decimal = split.indexOf(".");
-     int minutes = Integer.parseInt(split.substring(0,decimal));
-     double seconds = Double.parseDouble(split.substring(decimal))*60;
-        String fsplit = String.format("%d:%06.3f", minutes, seconds); 
+        String split = (m1/60) + "";
+        // Divide the double by 60 to get the number of minutes, which will turn up to be a number with many decimals afterwards(eg. 5.32323412) 
+        // turn it into a string so that it will be easier to seperate the digits before and after the decimal so that the proper operations can be performed
+
+        int decimal = split.indexOf(".");
+        int minutes = Integer.parseInt(split.substring(0,decimal));
+        // the number of minutes would be equal to the numbers from the beginning up until the decimal point in the string
+        double seconds = Double.parseDouble(split.substring(decimal))*60;
+        // the seconds would need to be calculated by taking all the numbers after the decimal, and multiplying it by 60 to find the seconds
+        String fsplit = String.format("%d:%06.3f", minutes, seconds ); 
+        // .format is needed to correctly format the string properly with the ":" in the right place 
+        // it also prevents issues like 3:6.125123123 where a 0 needs to be added on the right of the "6" and the decimals need to be limited to 3 spots
+        // %d:%06.3f - tells that the String will be formated so that the integer (d%) will be put first on the string, then followed by ":" 
+        // it will then put a 0, then specifies that the string will be at lest 6 characters, and it can only have 3 characters after the decimal (".")
         
-        
-        
-        return fsplit; 
+    return fsplit; 
     }
 
+// gets the time which is in typical M:S.M form (string), and converts it into seconds. returns a double
     public static double getSec(String m){
         int colon = m.indexOf(":");
+        //finding the index of the colon so that it can be used to seperate the string by minutes and seconds
         int minutes = Integer.parseInt(m.substring(0,colon)) *60;
-     double seconds = Double.parseDouble(m.substring(colon+1));
-
+        // takes all the characters between 0 and colon and multiplies it by 60 to convert minutes to seconds
+        double seconds = Double.parseDouble(m.substring(colon+1));
+        // takes all the numbers after the colon, no math needs to be done since the numbers are already seconds
         double s = minutes + seconds; 
 
      return s;
 
 
     }
-
-    private static void runnerTime( String m1, String m2, String m3 ) {
-
-
-     
-     double split1 = getSec(m1);
-         
-     double split2 = getSec(m2) + split1;
+    // calls other functions like getsec and getSplit and displays all the information to the runner
+    private static void runnerTime( String m1, String m2, String m3, String fName, String lName ) {
+        double split1 = getSec(m1);
+         // uses method getSec to convert the string into seconds so that it can be added and subtracted to find the splits
+        double split2 =  getSec(m2)- split1 ;
     
-     double split3 = getSec(m3) - split2;
+        double split3 = getSec(m3) - (split2 + split1);
 
-     System.out.println("Name of runner:" + fName +" " + lName);
+        System.out.println("Summary of Runner "  + runNum + ": "+ fName +" " + lName);
 
-    System.out.println("Split one: " + getSplit(split1) + " min");
-    System.out.println("Split two: " + getSplit(split2) + " min" );
-    System.out.println("Split three: " + getSplit(split3) + " min");
-    System.out.println ("Total Time: " + getSplit(split3 + split2) + " min");
-
+        System.out.println("Split one: " + getSplit(split1) + " min");
+        System.out.println("Split two: " + getSplit(split2) + " min" );
+        System.out.println("Split three: " + getSplit(split3) + " min");
+        System.out.println ("Total Time: " + getSplit(split3 + split2 + split1) + " min\r\n");
+        // prints back out a summary of the data
      
 
 
@@ -71,24 +74,26 @@ public class CrossCountryAssignment {
     
 
     }
-
+    // gets all the information that is required by the runner to input, and uses scanners to assign the data inputed to variables
     public static void runnerData(Scanner in) {
-      
-        
-        System.out.println("Please enter your first name:");
-     fName = in.nextLine(); 
-
+        runNum++;
+        // runNum is used here
+        System.out.println("Runner #" + runNum +" please enter your first name:");
+        String fName = in.nextLine(); 
+        //scanner gets the keyboard inputs and stores them into the variables so that it can be passed onto the runnerTime method 
         System.out.print("Please enter your last name: \r\n" );
-     lName = in.nextLine();
+        String lName = in.nextLine();
 
-    System.out.print( "Welcome " + fName + " " + lName + " please enter your first mile time in the format (Minute:Seconds.Milliseconds) \r\n" );
-        mile1 = in.nextLine();
+        System.out.print( "Welcome " + fName + " " + lName + ", please enter the time when you completed the first mile in the format (Minute:Seconds.Milliseconds) " );
+        String mile1 = in.nextLine();
 
-   System.out.println("Next, enter your time for your second mile time: \r\n");
-         mile2 = in.nextLine();
+        System.out.println("Next, enter the time when you completed the second mile ");
+        String mile2 = in.nextLine();
 
-   System.out.println("Finally, please enter the total time of the run\r\n");
-        mile3 = in.nextLine();
+        System.out.println("Finally, please enter the total time of the run");
+        String mile3 = in.nextLine();
+
+       runnerTime(mile1, mile2, mile3, fName, lName);
         
     }
     
